8f3a92e3c94730306b98a6da2943c36eb1f04d20
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-08-15 16:51:09 -0500

    subiendo 360

---
+++ js/nav-scroll.js
@@ -0,0 +1,17 @@
+// Closes the Responsive Menu on Menu Item Click
+/* navbar */
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background-color', '#2A3541');
+       } else {
+          $('.navbar-default').css('background-color', 'transparent');
+       }
+   });
+    }
+});
